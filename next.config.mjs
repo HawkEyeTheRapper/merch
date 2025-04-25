7a84053cad9ea69b000817b6f1769ebb6bad7188
HawkEyeTheRapper <166974712+HawkEyeTheRapper@users.noreply.github.com>
2025-04-25 13:04:02 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    unoptimized: true, // Helpful for Cloudflare-hosted assets
+  },
+};
 
 export default nextConfig;
